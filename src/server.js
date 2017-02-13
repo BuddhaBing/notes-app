74a73f3cd8fe4a821d18e26c5eafc890c55e34b0
Joe Marriott <marriott90@gmail.com>
2017-02-13 16:10:02 +0000

    node serves up index.html (no css)

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 /*jshint esversion: 6 */
 
+var fs = require("fs");
 const http = require ('http');
 const port = 8080;
 
@@ -8,7 +9,13 @@ const requestHandler = (request, response) => {
   response.end('Hello Node.js Server!');
 };
 
-const server = http.createServer(requestHandler);
+const server = http.createServer(function(requestHandler, response){
+  fs.readFile("index.html", function(err, data){
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write(data);
+    response.end();
+  });
+}).listen(port);
 
 server.listen(port, (err) => {
   if (err) {
